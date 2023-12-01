36a5087be9853d17c02a18c386b42e2f0c7467fd
wesleyjose639@gmail.com <wesleyjose639@gmail.com>
2023-12-01 07:01:16 -0300

    att

--- produto.php
+++ produto.php
@@ -3,12 +3,19 @@
    <head>
       <meta charset="utf-8"> 
       <meta name="viewport" content="width=device-width"> 
-      <title>Mirror Fashion</title> 
+      <title>NAMO</title> 
       <link rel="stylesheet" href="css/reset.css"> 
       <link rel="stylesheet" href="css/estilos.css">
       <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)"> 
       </head> 
       <body>
         <?php include (cabecalho.php); ?>
+        
+        <?php
+        $cabecalho_title = "Produto da NAMO"; include("cabecalho.php");
+         ?>
+        
+        <?php include (rodape.php); ?>
+        
          </body> 
          </html>
