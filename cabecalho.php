cf00ce188118f676622212f20322d27f463a018f
Wesleyjoseph <wesleyjose639@gmail.com>
2024-03-13 08:41:01 -0300

    update

---
+++ cabecalho.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title><?php  print $cabecalho_title; ?></title>
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/estilos.css">
+        <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
+        <meta name="viewport" content="width=device-width">
+        </head>
+        <body>  
+        <header class="container">  
+        <!-- conteúdo do header aqui -->  
+        </header>
+        
+
+  <header>
+  <div class="menu-opcoes"> 
+  <ul>  
+  <li><a href="conta.html">Conta</a></li>  
+  <li><a href="desejo.html">Desejos</a></li> 
+  <li><a href="sobre.html">Sobre</a></li> 
+  <li><a href="ajuda.html">Ajuda</a></li>  
+  </ul> 
+  </div> 
+  </header>
