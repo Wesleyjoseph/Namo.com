ff06cd24423909d5d3a07c8a2eda39a2429dbe67
Wesleyjoseph <wesleyjose639@gmail.com>
2024-03-19 06:51:59 -0300

    AT

--- rodape.php
+++
@@ -1,8 +0,0 @@
-  <footer>
-  <div class="text"> 
-  <div class="site">CASUAL</div>  
-  <div class="address">  Rua Madalena, <br>   
-  153 SAO PAULO, CAPITAL<br> 
-  +55 11 *****-****
-  </div> 
-</footer>
